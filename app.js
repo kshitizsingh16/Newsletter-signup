93b7e50524d2aec4475ae30c10cda814942d8c95
= <=>
2024-06-14 12:47:50 +0530

    Modify app.js

--- app.js
+++ app.js
@@ -33,9 +33,6 @@ app.post("/", function (req, res) {
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) res.sendFile(__dirname + "/success.html");
     else res.sendFile(__dirname + "/failure.html");
-    response.on("data", function (data) {
-      console.log(JSON.parse(data));
-    });
   });
   request.write(Jasondata);
   request.end();
